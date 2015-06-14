cb8c98d9f0b061df8a73015226ba4d37d0999c4f
Chris Hersh <ch5968@cs.ship.edu>
2015-06-13 20:26:11 -0400

    Added the renamer as I have it currently

---
+++ plexRenamer.py
@@ -0,0 +1,104 @@
+from os import listdir
+from os.path import join, isfile
+import sys
+import re
+
+#dryRun will not change any files, it will print out what it would have changed
+dryRun = True
+
+#Change to fit needs, if no subtitle files set boolean below to false
+videoFiletype = "mkv"
+subtitleFiletype = "ass"
+
+#Are there any subtitle files (.ass/.sas) that should be renamed?
+renameSubtitleFiles = True
+
+#This will be before the season and episode count
+# eg: Ginatama-s01e003
+renamedFilePrefix = "Gintama - "
+
+#This is used to make the regex more likely to grab the correct number from the title
+currentEpisodePrefix = "[RG Genshiken] Gintama - "
+
+episodeNumberLength = 3
+
+#episodeRegex will get the episode number for normal episodes
+#multiRegex will catch episodes with multiple episodes
+episodeRegex = re.compile(r"" + re.escape(currentEpisodePrefix) + r"(\d{" + str(episodeNumberLength) + r"}).*")
+multiRegex = re.compile(r"" + re.escape(currentEpisodePrefix) + 
+	r"(\d{" + str(episodeNumberLength) + r"}-\d{" + str(episodeNumberLength) + r"}).*")
+print(str(episodeRegex))
+
+if len(sys.argv) != 2:
+	print("Please give a path to the root of the series")
+	sys.exit()
+
+basePath = sys.argv[1];
+
+
+def renameFile(seasonCounter, seasonPath, currEpisodeName, epNum):
+	newName = renamedFilePrefix + "s" + str(seasonCounter) + "e" + epNum
+	if(dryRun):
+		print(currEpisodeName + " --> " + newName)
+	else:
+		os.rename(join(seasonPath, currEpisodeName), join(seasonPath, newName))
+
+
+def printError(msg):
+	sys.stderr.write(msg)
+
+def printProcessingError(episode, season):
+	printError("Could not process file: " + episode + "\n\tIn directory: " + season + "\n")
+
+
+def renameFiles(filetype, seasonDirs):
+	#TODO
+	seasonCounter = 0
+	for s in seasonDirs:
+		seasonPath = join(basePath, s)
+		seasonCounter += 1
+
+		#get all episodes in the current directory that are of the proper filetype
+		episodes = [ f for f in listdir(seasonPath) if ( isfile(join(seasonPath,f)) and (f.endswith(videoFiletype)) ) ]
+		episodes.sort()
+
+		for e in episodes:
+			#Try the multi first as it's the most restrictive
+			try:
+				epNum = multiRegex.match(e).group(1)
+				renameFile(seasonCounter, seasonPath, e, epNum)
+			#Then see if it's a valid normal episode
+			except AttributeError:
+				#Python cannot catch any errors in this except without another try block
+				try:
+					epNum = episodeRegex.match(e).group(1)
+					renameFile(seasonCounter, seasonPath, e, epNum)
+					#Finally warn user of invalid file and where it's located
+				except:
+					printProcessingError(e, s)
+			except:
+				printProcessingError(e, s)
+
+
+
+#Get all directories, ignore files
+def runRename():
+	seasonDirs = [ f for f in listdir(basePath) if (not isfile(join(basePath,f))) ]
+	seasonDirs.sort()
+
+	print(len(seasonDirs))
+
+	#Rename the video files and the subtitle files if applicable
+	renameFiles(videoFiletype)
+
+	if(renameSubtitleFiles):
+		try:
+			renameFiles(subtitleFiletype)
+		except:
+			printError("Could not process subtitle files correctly")
+
+
+
+
+if __name__ == "__main__":
+	runRename()
